cd33cb60e10f9b96be7692f3a1af15e04496a408
Anne Lowell <anneklowell@outlook.com>
2017-12-05 16:52:22 -0600

    user signup studio 2 complete

--- cheese-mvc/src/main/java/com/example/cheesemvc/controllers/UserController.java
+++ cheese-mvc/src/main/java/com/example/cheesemvc/controllers/UserController.java
@@ -4,9 +4,12 @@ package com.example.cheesemvc.controllers;
 import com.example.cheesemvc.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -17,19 +20,31 @@ public class UserController {
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
 
+        model.addAttribute(new User());
+
         return "user/add";
     }
 
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @ModelAttribute User user, String verify) {
-        if (user.getPassword().equals(verify)) {
-            model.addAttribute("username", user.getUsername());
-            return "user/index";
-        } else {
+    public String add(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        if (errors.hasErrors()) {
+            model.addAttribute(user);
             return "user/add";
         }
-    }
-    }
+
+        if (!(user.getPassword().equals(verify)) || ((verify.isEmpty()) || (user.getPassword().isEmpty()))) {
+            model.addAttribute("error", "Password and verify must be filled in and match.");
 
 
+            return "user/add";
+    }
+
+        else {
+            model.addAttribute("username", user.getUsername());
+            return "user/index";
+        }
+
+    }
+
+}
