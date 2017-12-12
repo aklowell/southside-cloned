d54ef3909457092a0623a4fbef4b0e21f6d3cb02
Anne Lowell <anneklowell@outlook.com>
2017-12-12 12:59:41 -0600

    bookgroup?

---
+++ bookgroup/main/src/org/launchcode/BookGroupApplication.java
@@ -0,0 +1,10 @@
+package org.launchcode;
+
+
+@SpringBootApplication
+public class BookGroupApplication {
+
+    public static void main(String[] args) {
+        Spring.Application.run(BookGroupApplication.class,args);
+    }
+}
